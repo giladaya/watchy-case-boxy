ace8f4156664ac50e3bdbf70fa2412b4e1103d33
Gilad <giladaya@gmail.com>
2021-07-17 13:26:17 +0300

    initial

---
+++ boxy_case.py
@@ -0,0 +1,199 @@
+# import importlib
+# import cadquery as cq
+from watchy_sizes import *
+import watchy_sizes
+
+# New params
+p_tolerance = 0.5
+p_ledge_h = pcb_y_to_slot + pcb_slot_h
+p_strip_width = 24 + 0.5
+p_strip_dia = 4.0
+p_tbar_space_height = p_strip_dia + 1.0
+p_tbar_hole_r = 0.5
+p_under_pcb_depth = 8.0
+p_inset_depth = pcb_t / 2.0
+
+#parameter definitions
+p_thickness =  1.0 #Thickness of the box walls
+
+p_outerWidth = pcb_w + 2 * p_thickness + p_tolerance #Outer width of box enclosure
+p_outerLength = pcb_h + 2 * p_thickness + p_tolerance #Outer length of box enclosure
+p_outerHeight = p_under_pcb_depth + p_thickness + p_inset_depth#Outer height of box enclosure
+
+p_sideRadius = pcb_radius #Radius for the curves around the sides of the box
+p_topAndBottomRadius =  p_outerHeight * 0.6 #Radius for the curves on the top and bottom edges of the box
+p_topAndBottomRadiusInner =  p_topAndBottomRadius
+
+p_screwpostID = 2.5 #Inner Diameter of the screw post holes, should be roughly screw diameter not including threads
+
+p_boreDiameter = 4.5 #Diameter of the counterbore hole, if any
+p_boreDepth = 0 #Depth of the counterbore hole, if
+p_countersinkDiameter = 0.0 #Outer diameter of countersink.  Should roughly match the outer diameter of the screw head
+p_countersinkAngle = 90.0 #Countersink angle (complete angle between opposite sides, not from center to one side)
+p_flipLid = True #Whether to place the lid with the top facing down or not.
+p_lipHeight =  1.0 #Height of lip on the underside of the lid.\nSits inside the box body for a snug fit.
+
+#outer shell
+oshell = (cq.Workplane("XY")
+  .rect(p_outerWidth,p_outerLength)
+  .extrude(p_outerHeight)
+)
+
+#debug(oshell.edges(cq.NearestToPointSelector((0,0,0))))
+#weird geometry happens if we make the fillets in the wrong order
+if p_sideRadius > p_topAndBottomRadius:
+    oshell = oshell.edges("|Z").fillet(p_sideRadius)
+    oshell = oshell.edges("#Z").fillet(p_topAndBottomRadius)
+else:
+    #oshell = oshell.edges("#Z").fillet(p_topAndBottomRadius)
+    #oshell = oshell.edges(cq.NearestToPointSelector((0,0,0))).fillet(p_topAndBottomRadius)
+    oshell = oshell.edges("#Z and(not(>Z))").fillet(p_topAndBottomRadius)
+    oshell = oshell.edges("|Z").fillet(p_sideRadius)
+
+#inner shell
+ishell = (oshell.faces("<Z").workplane(p_thickness,True)
+    .rect((p_outerWidth - 2.0* p_thickness),(p_outerLength - 2.0*p_thickness - 2.0*p_ledge_h))
+    .extrude((p_outerHeight - p_thickness),False) #set combine false to produce just the new boss
+)
+ishell = (ishell.edges("|Z")
+  .fillet(p_sideRadius - p_thickness)
+  #.edges("#Z and >Z[1]").fillet(p_topAndBottomRadius)
+  .edges(cq.NearestToPointSelector((0,0,0)))
+  .fillet(p_topAndBottomRadiusInner)
+)
+
+#make the box outer box
+box = oshell.cut(ishell)
+
+# Top strip hole
+tbar_hole_depth = 1.5
+tbar_top = (cq.Workplane("ZY")
+  .workplane(origin=(0, p_outerLength / 2.0 - p_topAndBottomRadius + p_strip_dia, 1.5), offset=(-p_strip_width / 2.0))
+  .circle(p_strip_dia)
+  .extrude(p_strip_width)
+  .workplane(origin=(0, p_outerLength / 2.0 - p_topAndBottomRadius + p_strip_dia / 2.0 + 0.5, p_strip_dia / 2.0 + 0.5), offset=(-p_strip_width / 2.0 - tbar_hole_depth))
+  .circle(p_tbar_hole_r)
+  .extrude(p_strip_width + tbar_hole_depth * 2.0)
+)
+tbar_bottom = (cq.Workplane("ZY")
+  .workplane(origin=(0, -(p_outerLength / 2.0 - p_topAndBottomRadius + p_strip_dia), 1.5), offset=(-p_strip_width / 2.0))
+  .circle(p_strip_dia)
+  .extrude(p_strip_width)
+  .workplane(
+    origin=(0, -(p_outerLength / 2.0 - p_topAndBottomRadius + p_strip_dia / 2.0 + 0.5), p_strip_dia / 2.0 + 0.5), 
+    offset=(-p_strip_width / 2.0 - tbar_hole_depth))
+  .circle(p_tbar_hole_r)
+  .extrude(p_strip_width + tbar_hole_depth * 2.0)
+)
+with_tbars = box.cut(tbar_top).cut(tbar_bottom)
+
+# side holes (buttons, etc)
+offset_to_button_1 = 5.7
+offset_to_button_2 = 32.5
+button_width  = 6.7
+button_height = 2.0
+usb_b_width  = 8.4
+usb_b_height = 3.0
+distance_to_usb = 17.9
+vib_motor_width  = 11.0
+vib_motor_height = 2.8
+
+holes_left = (cq.Workplane("YZ")
+  .moveTo(p_outerLength / 2.0 - p_thickness - offset_to_button_1, p_outerHeight)
+  .vLine(-button_height)
+  .hLine(-button_width - 6.5)
+  .vLine(-(usb_b_height - button_height))
+  .hLine(-usb_b_width)
+  .vLine(usb_b_height - button_height)
+  .hLine(-6.5 - button_width)
+  .vLine(button_height)
+  .close()
+  .extrude(-p_outerWidth / 2.0)
+)
+holes_right = (cq.Workplane("YZ")
+  .moveTo(p_outerLength / 2.0 - p_thickness - offset_to_button_1, p_outerHeight)
+  .vLine(-button_height)
+  .hLine(-button_width - 2.0)
+  .vLine(-(vib_motor_height - button_height))
+  .hLine(-vib_motor_width)
+  .vLine(vib_motor_height - button_height)
+  .hLine(-6.0 - button_width)
+  .vLine(button_height)
+  .close()
+  .extrude(p_outerWidth / 2.0)
+)
+
+with_side_holes = with_tbars.cut(holes_left).cut(holes_right)
+
+# pcb inset
+pcb_inset = (cq.Workplane("XY")
+  .workplane(offset=p_outerHeight)
+  .rect(pcb_w, pcb_h)
+  .extrude(-p_inset_depth)
+  .edges("|Z")
+  .fillet(pcb_radius)
+)
+#debug(pcb_inset)
+with_inset = with_side_holes.cut(pcb_inset)
+
+# fasteners
+fastener_overhang = 0.5
+fastener_height = fastener_overhang + p_thickness + p_thickness
+fastener_depth = 5.0
+fastener_hole_point = (0, p_outerHeight - p_thickness - p_screwpostID / 2.0 + 0.2)
+
+fastener_top = (cq.Workplane("XY")
+  .workplane(
+    origin=(0, (p_outerLength / 2.0 - fastener_height / 2.0) + p_thickness, 0), 
+    offset=(p_outerHeight + p_thickness + pcb_t - p_inset_depth))
+  .rect(pcb_w * 0.5, fastener_height)
+  .extrude(-fastener_depth)
+  .edges("|Z or(#Z)")
+  .fillet(1.0)
+  .faces("#X and >X")
+  .workplane(
+    origin=(0, (p_outerLength / 2.0 - fastener_height / 2.0), 0), 
+    offset=(-p_thickness))
+  .rect(pcb_w * 0.5, fastener_height)
+  .cutBlind(-fastener_depth)
+  .faces("|Y and >Y")
+  .workplane(origin=(0, 0, 0))
+  .pushPoints( [ fastener_hole_point])
+  #.cskHole(p_fastener_screw_dia, 2.0 * p_fastener_screw_dia, 82)
+  .cboreHole(p_screwpostID, p_boreDiameter, p_boreDepth)
+)
+
+fastener_bottom = (cq.Workplane("XY")
+  .workplane(
+    origin=(0, -(p_outerLength / 2.0 - fastener_height / 2.0) - p_thickness, 0), 
+    offset=(p_outerHeight + p_thickness + pcb_t - p_inset_depth))
+  .rect(pcb_w * 0.5, fastener_height)
+  .extrude(-fastener_depth)
+  .edges("|Z or(#Z)")
+  .fillet(1.0)
+  .faces("#X and >X")
+  .workplane(
+    origin=(0, -(p_outerLength / 2.0 - fastener_height / 2.0) + p_thickness, 0), 
+    offset=(-p_thickness))
+  .rect(pcb_w * 0.5, fastener_height)
+  .cutBlind(-fastener_depth)
+)
+
+fastener_top = (fastener_top
+  .translate((0, fastener_depth, -p_outerHeight + fastener_height / 2.0))
+  .rotateAboutCenter((1,0,0), -90)
+)
+#debug(fastener_top)
+
+with_fastener = (with_inset
+  .union(fastener_bottom)
+  .faces("|Y and >Y")
+  .workplane(origin=(0, 0, 0))
+  .pushPoints( [ fastener_hole_point])
+  .hole(p_screwpostID, p_outerLength / 2.0)
+)
+result = with_fastener.union(fastener_top)
+
+#return the combined result
+show_object(result)
+cq.exporters.export(result, "boxy.stl")
---
+++ watchy_sizes.py
@@ -0,0 +1,12 @@
+pcb_w = 33.8
+pcb_h = 46.0
+pcb_t = 1
+pcb_slot_x = 4.1
+pcb_y_to_slot = 1.9
+pcb_slot_h = 2.0
+pcb_slot_w = 4.1
+pcb_radius = 3.5
+screen_w = 31.8
+screen_h = 37.32
+screen_t = 1.05 + 0.05
+
